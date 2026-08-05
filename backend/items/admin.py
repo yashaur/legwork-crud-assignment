34b0556a9e77edbeaef87d2c3b771383f776841b
shaurya <work.shauryasingru@gmail.com>
2026-08-05 16:08:27 +0530

    create BaseModel and Items

--- backend/items/admin.py
+++ backend/items/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from items.models import Items
 
 # Register your models here.
+admin.site.register(Items)
--- backend/items/models.py
+++ backend/items/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class BaseModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(default = uuid4, editable = False, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Items(BaseModel):
+    key = models.CharField(max_length = 50, null=False, blank=False)
+    value = models.TextField()
+
+    def __str__(self):
+        return str(self.key)
