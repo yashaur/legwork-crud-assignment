3f1b8f2c48e55be62b9fb29cc5c486e6c86e0fc0
shaurya <work.shauryasingru@gmail.com>
2026-08-05 17:49:23 +0530

    bug fixes to seed.py

--- backend/items/management/commands/seed_items.py
+++ backend/items/management/commands/seed_items.py
@@ -14,10 +14,12 @@ class Command(BaseCommand):
         FILEPATH = settings.BASE_DIR.parent / 'data/items.json'
 
         if not FILEPATH.exists():
-            raise CommandError(f"No file called items.json found in {FILEPATH.parent}.\nPlease ensure that `items.json` is added to `backend/data/`")
+            raise CommandError(f"No file called items.json found in {FILEPATH.parent}.\nPlease ensure that `items.json` is added to `root/data/`")
 
-        file = json.loads(FILEPATH.read_text())
-        n_rows = len(file)
+        try:
+            file = json.loads(FILEPATH.read_text())
+        except Exception as e:
+            raise CommandError(f"Could not parse json due to the following error: {e}")
 
         if not isinstance(file,list):
             raise CommandError("Incorrect file format: must be a list containing key-value pairs")
@@ -26,6 +28,7 @@ class Command(BaseCommand):
             Items.objects.all().delete()
 
             bad_imports = 0
+            n_rows = len(file)
 
             for row in file:
                 if not isinstance(row, dict):
@@ -34,18 +37,21 @@ class Command(BaseCommand):
 
                 key, value = row.get('key'), row.get('value')
 
-                if not (key or value):
+                key_invalid = (key == "") or (key is None)
+                value_invalid = (value is None)
+
+                if key_invalid or value_invalid:
                     bad_imports += 1
                     continue
 
                 Items.objects.create(key=key, value=value)
 
         if bad_imports == 0:
-            self.style.SUCCESS(self.stdout.write(f"File written to database sucessfully. Added {n_rows} rows"))
+            self.stdout.write(self.style.SUCCESS(f"File written to database sucessfully. Added {n_rows} rows"))
         elif bad_imports > 0 and bad_imports < n_rows:
-            self.style.WARNING(self.stdout.write(f"File written to database but with skipped rows. {n_rows-bad_imports} written; {bad_imports} skipped."))
+            self.stdout.write(self.style.WARNING(f"File written to database but with skipped rows. {n_rows-bad_imports} written; {bad_imports} skipped."))
         else:
-            self.style.ERROR_OUTPUT(self.stdout.write(f"File could not be written to database"))
+            self.stdout.write(self.style.ERROR(f"File could not be written to database"))
             raise CommandError("File could not be written to database. Check the format") 
 
         
